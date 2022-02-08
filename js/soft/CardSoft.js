bad7681ebabd898c452321ae6e6136876a33df19
Bogdan <b@bmazur.ru>
2022-02-09 00:13:34 +0300

    fix ref soft

--- js/soft/CardSoft.js
+++ js/soft/CardSoft.js
@@ -8,12 +8,14 @@ export class CardSoft extends Card {
     this._item = item;
     this._element = settings.element;
     this._cardDescription = settings.elementDescription;
+    this._cardImage = settings.elementImage;
     this._cardLink = settings.elementLink;
     this._elementName = settings.elementName;
     this._editForm = document.querySelector(settings.editForm);
     this._fieldName = this._editForm.querySelector(settings.inputName);
     this._fieldDescription = this._editForm.querySelector(settings.inputDescription);
     this._fieldLink = this._editForm.querySelector(settings.inputLink);
+    this._fieldImage = this._editForm.querySelector(settings.inputImage);
     this._removeButton = settings.removeButton;
     this._editButton = settings.editButton;
     this._editCardClick = handleCardClick;
@@ -24,6 +26,7 @@ export class CardSoft extends Card {
     super._editCard(evt);
     this._fieldDescription.value = this._item.description;
     this._fieldLink.value = this._item.link;
+    this._fieldImage.value = this._item.image;
     this._editCardClick.open({currentCard: this._cardElement, item: this._item});
   }
 
@@ -36,6 +39,8 @@ export class CardSoft extends Card {
     super.createCard();
     this._cardElement.querySelector(this._cardDescription).textContent = this._item.description;
     this._cardElement.querySelector(this._cardLink).href = this._item.link;
+    this._cardElement.querySelector(this._cardImage).src = this._item.image;
+    this._cardElement.querySelector(this._cardImage).alt = this._item.name;
     return this._cardElement;
   }
 }
