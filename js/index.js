9ae1c1c16e7dcd0476073cc433dc528a65c7d829
BMAZUR <49879328+bmazurme@users.noreply.github.com>
2022-02-08 11:03:17 +0300

    fix index

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { settings } from "./config/settings";
+import { settings } from "./config/settings.js";
 
 const currentYear = (new Date()).getFullYear();
 const field = document.querySelector(settings.footerCopyright);
